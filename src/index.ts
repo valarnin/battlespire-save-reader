0c61e1880dcac12e973d4c75a0bb19cb812d8515
valarnin <valarnin@gmail.com>
2024-02-23 21:56:24 -0500

    Sort output blocks for easier reading

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import SaveTree from './SaveTree/SaveTree';
 import fs from 'fs';
 import { argv } from 'process';
 import { Util } from './Util/Util';
+import { AnyBlock } from './SaveTree/BlockTypeMappings';
 
 let filePath: string | undefined = undefined;
 
@@ -22,4 +23,8 @@ const buffer = file.buffer;
 
 const parsed = new SaveTree();
 parsed.deserializeFrom(Buffer.from(buffer));
+// For sanity's sake, apply some sorting before we dump it
+const playerEntry = parsed.blocks.find(b => 'ID' in b ? b.ID === 50000 : false) as AnyBlock;
+const blocks = parsed.blocks.slice(1).sort((b1, b2) => ('ID' in b1 && 'ID' in b2) ? b1.ID - b2.ID : 0);
+parsed.blocks = [playerEntry, ...blocks];
 console.log(Util.strippedStringify(parsed));
